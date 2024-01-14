3553eb073a10d549fd07ee9f09b0d09f6be0b92a
NSarwar <nuhash.sarwar@gmail.com>
2024-01-14 12:58:36 -0500

    Added gitignore to the wrong folder

--- src/cypress/e2e/e2e.cy.js
+++ src/cypress/e2e/e2e.cy.js
@@ -12,10 +12,15 @@ describe('Cypress Website Demo Test', () => {
         cy.contains('Weekly downloads').should('be.visible').then(($weeklyDownloads) => {
             const weeklyDownloadsNumber = $weeklyDownloads.siblings();
 
+            // Check for number beside weekly downloads
             const weeklyDownloadsNumberText = weeklyDownloadsNumber.text()
             const weeklyDownloadsNumberValue = parseInt(weeklyDownloadsNumberText, 10)
 
             expect(weeklyDownloadsNumberValue).to.be.a('number')
         })
+    })
+
+    it('User is able to click on Company and then on "About Cypress" => Success', () =>{
+
     })
 })
